2d9beded39470366c4e59d5aef4a7abd4db5c9a5
Curtis Salinger <csalinger22@cmc.edu>
2020-02-20 10:39:17 -0800

    Word ladder attempt 1

--- word_ladder.py
+++ word_ladder.py
@@ -1,49 +1,98 @@
 #!/bin/python3
-
+import copy
+from collections import deque
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
-    '''
-    Returns a list satisfying the following properties:
+	'''
+	Returns a list satisfying the following properties:
 
-    1. the first element is `start_word`
-    2. the last element is `end_word`
-    3. elements at index i and i+1 are `_adjacent`
-    4. all elements are entries in the `dictionary_file` file
+	1. the first element is `start_word`
+	2. the last element is `end_word`
+	3. elements at index i and i+1 are `_adjacent`
+	4. all elements are entries in the `dictionary_file` file
 
-    For example, running the command
-    ```
-    word_ladder('stone','money')
-    ```
-    may give the output
-    ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
-    ```
-    but the possible outputs are not unique,
-    so you may also get the output
-    ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
-    ```
-    (We cannot use doctests here because the outputs are not unique.)
+	For example, running the command
+	```
+	word_ladder('stone','money')
+	```
+	may give the output
+	```
+	['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
+	```
+	but the possible outputs are not unique,
+	so you may also get the output
+	```
+	['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
+	```
+	(We cannot use doctests here because the outputs are not unique.)
 
-    Whenever it is impossible to generate a word ladder between the two words,
-    the function returns `None`.
-    '''
+	Whenever it is impossible to generate a word ladder between the two words,
+	the function returns `None`.
+	'''
+	"""
+	Create a stack
+	Push the start word onto the stack
+	Create a queue
+	Enqueue the stack onto the queue
+
+	While the queue is not empty
+		Dequeue a stack from the queue
+		For each word in the dictionary
+			If the word is adjacent to the top of the stack
+			If this word is the end word
+				You are done!
+				The front stack plus this word is your word ladder.
+			Make a copy of the stack
+			Push the found word onto the copy
+			Enqueue the copy
+			Delete word from the dictionary
+			"""
+	start = start_word
+	end = end_word
+	ladder = []
+	q = deque([])
+	ladder.append(start)
+	q.append(ladder)
+	with open(dictionary_file) as dtc:
+		words = dtc.readlines()
+		while q != []:
+			for word in words:
+				if _adjacent(word,ladder[-1]):
+					if word == end:
+						ladder.append(word)
+						return ladder
+					new = copy.copy(ladder)
+					q.append(new)
+					words.remove(word)
+
 
 
 def verify_word_ladder(ladder):
-    '''
-    Returns True if each entry of the input list is adjacent to its neighbors;
-    otherwise returns False.
-    '''
-
+	'''
+	Returns True if each entry of the input list is adjacent to its neighbors;
+	otherwise returns False.
+	'''
+	for i in range(len(ladder) - 1):
+		if not  _adjacent(ladder[i],ladder[i+1]):
+			return False
 
 def _adjacent(word1, word2):
-    '''
-    Returns True if the input words differ by only a single character;
-    returns False otherwise.
+	'''
+	Returns True if the input words differ by only a single character;
+	returns False otherwise.
+	>>> _adjacent('phone','phony')
+	True
+	>>> _adjacent('stone','money')
+	False
+	'''
+	count = 0
+	for i in range(len(word1)-1):
+		if word1[i] != word2[i]:
+			count += 1
+	if count == 1:
+		return True
+	else:
+		return False
 
-    >>> _adjacent('phone','phony')
-    True
-    >>> _adjacent('stone','money')
-    False
-    '''
+
+
